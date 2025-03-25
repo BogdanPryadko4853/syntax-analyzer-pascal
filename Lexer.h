f50a40f2b9ff6db309e46b71e08e8121cc869abc
Bogdan Pryadko <bogdanpryadko1@gmail.com>
2025-03-25 23:47:05 +0300

    init module Lexer

---
+++ Lexer.h
@@ -0,0 +1,23 @@
+#ifndef SYNTAX_ANALYZER_LEXER_H
+#define SYNTAX_ANALYZER_LEXER_H
+
+#include <string>
+#include <vector>
+#include "BoundedDeque.h"
+
+constexpr size_t LOOKAHEAD_BUFFER_SIZE = 5;
+
+enum class TokenType {
+
+};
+
+struct  Token{
+
+};
+
+class Lexer{
+
+};
+
+
+#endif
