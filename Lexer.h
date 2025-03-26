c73273b97d609b5398b580967427a6cc8d7aef6f
Bogdan Pryadko <bogdanpryadko1@gmail.com>
2025-03-26 09:18:41 +0300

    ✨  feat Lexer.h

--- Lexer.h
+++ Lexer.h
@@ -1,23 +1,61 @@
-#ifndef SYNTAX_ANALYZER_LEXER_H
-#define SYNTAX_ANALYZER_LEXER_H
+#ifndef LEXER_H
+#define LEXER_H
 
 #include <string>
 #include <vector>
+#include <stdexcept>
 #include "BoundedDeque.h"
 
 constexpr size_t LOOKAHEAD_BUFFER_SIZE = 5;
 
 enum class TokenType {
 
+    CONST, VAR, BEGIN, END, TEMPLATE, CLASS, TYPENAME,
+    ASSERT, WHILE, IF, ELSE, DO, READLN, WRITELN, WRITE, THEN,
+
+    PLUS, MINUS, TIMES, DIVIDE,
+    EQ, NE, LT, GT, LE, GE,
+    ASSIGN, COLON,
+
+    LPAREN, RPAREN, LBRACE, RBRACE,
+    COMMA, SEMI, DOT,
+
+    NUMBER, STRING_LITERAL, IDENT,
+
+    END_OF_FILE, UNKNOWN
 };
 
-struct  Token{
+struct Token {
+    TokenType type;
+    std::string lexeme;
+    double value;
+    size_t line;
 
+    Token(TokenType type, const std::string& lexeme, double value = 0.0,
+          size_t line = 1)
+            : type(type), lexeme(lexeme), value(value), line(line) {}
 };
 
-class Lexer{
+class Lexer {
+public:
+    Lexer(const std::string& input);
+    std::vector<Token> tokenize();
 
+private:
+    std::string input;
+    size_t pos;
+    size_t length;
+    size_t currentLine;
+    BoundedDeque<char, LOOKAHEAD_BUFFER_SIZE> lookaheadBuffer;
+
+    char currentChar();
+    char peekAhead(size_t n = 1);
+    void advance();
+    void skipWhitespace();
+    Token number();
+    Token identifier();
+    Token getNextToken();
+    void fillLookahead(size_t n);
 };
-
 
 #endif
